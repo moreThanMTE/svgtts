5e970c739eaaaa7076bc5afeba1de83e809e05f0
moreThanMTE <lijianlin050416@gmail.com>
2024-11-20 00:15:08 +0800

    init

---
+++ index.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+
+import fs from 'fs'
+
+const inputPath = process.argv[2]
+const outputPath = process.argv[3] || './output.tsx'
+
+if (!inputPath) {
+  console.error('Usage: trans <input.svg> [output.tsx]')
+  process.exit(1)
+}
+
+const svgContent = fs.readFileSync(inputPath, 'utf-8')
+
+// 匹配并删除 height 和 width 属性，同时保留其他属性
+const modifiedSvgContent = svgContent.replace(
+  /<svg([^>]*)>/,
+  (match, group1) => {
+    // 移除 height 和 width 属性
+    const cleanedAttributes = group1.replace(/\s*(height|width)=["'][^"']*["']/g, '')
+    // 返回替换后的 <svg> 标签，添加动态 height 和 width
+    return `<svg${cleanedAttributes} height={heightPx} width={widthPx}>`
+  }
+)
+
+const tsxContent = `
+import React from 'react'
+
+export default function SvgComponent({ height, width }: { height: number, width: number }) {
+  const heightPx = height.toString() + 'px'
+  const widthPx = width.toString() + 'px'
+  return (
+    ${modifiedSvgContent}
+  )
+}
+`
+
+fs.writeFileSync(outputPath, tsxContent)
+console.log(`Converted ${inputPath} to ${outputPath}`)
---
+++ readme.md
@@ -0,0 +1,12 @@
+# SVGTTS
+Turn SVGs into TSX components, also allowing you to customize their height and width.
+
+## how to use?
+```npx svgtts [your svg]```
+
+this will auto turn the svg into ./output.tsx
+
+when using this component
+```tsx
+<YourComponent width={number} height={number}>
+```
